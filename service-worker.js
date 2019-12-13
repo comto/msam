ccd65c466b7c90a5f937aa6d075fc4ab084547e8
comto <49017794+comto@users.noreply.github.com>
2019-12-13 14:57:54 +0000

    Deploying to master from dev 9035cb1c6814e113ebb772384a57a29a60e3de29

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1e4b69de386e0a50d928359e2130b4dd.js"
+  "/msam/precache-manifest.fb3def60740e00ec43ddff6c9c994cd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/msam/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
