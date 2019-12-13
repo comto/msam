0b985419b23d6d997fdcc51d45aa62142db214c4
comto <libing780805@sina.com>
2019-12-13 22:47:35 +0800

    add build folder to git

---
+++ build/precache-manifest.1e4b69de386e0a50d928359e2130b4dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfe5e53f0b98147fc2bdaa1aec3137b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87fd400b0c44f80b7456",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3172c5b5f78b19c51df0",
+    "url": "/static/js/2.7bee919a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7bee919a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87fd400b0c44f80b7456",
+    "url": "/static/js/main.3c9c2fa3.chunk.js"
+  },
+  {
+    "revision": "8a8e975024bbe4fda2ad",
+    "url": "/static/js/runtime-main.db9cf02f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
